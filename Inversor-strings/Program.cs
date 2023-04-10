68b56ff54f53de498b1c634afbf8949944cb04d0
Lara Gabriele <laragboliver@gmail.com>
2023-04-10 18:24:49 -0300

    Finalização do teste inversor de strings

---
+++ Inversor-strings/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Insira uma palavra: ");
+string palavra = Console.ReadLine().ToUpper();
+
+string palavraInvertida = string.Empty;
+
+//separar os caracteres da palavra do fim para o começo
+for (int i = palavra.Length - 1; i >= 0; i--)
+{
+    //concatenar os caracteres
+    palavraInvertida += palavra[i];
+}
+
+Console.WriteLine("A palavra digitada foi " + palavra + " e ao contrário ela fica " + palavraInvertida + ".");
+
+Console.ReadLine();
